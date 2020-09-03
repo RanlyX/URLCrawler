38e78353359121d8b02d843d48ac1af41cf200a0
RanlyX <et82122@gmail.com>
2020-09-03 21:17:25 +0800

    To get a webpage.

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 # !usr/bin/python
 # -*- coding:utf-8 -*-
+import requests
 
-print("Hello python!")
+# To get a webpage.
+def main():
+    url = "https://www.phishtank.com/phish_detail.php?phish_id="
+    res = requests.get(url+"15")
+    return res.text
+
+if __name__ == '__main__':
+    print(main())
