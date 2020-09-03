a4d53af3768eb53083dbcbdf97499e77232f550a
RanlyX <et82122@gmail.com>
2020-09-03 21:26:48 +0800

    Check URL existed.

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 # !usr/bin/python
 # -*- coding:utf-8 -*-
 import requests
+from bs4 import BeautifulSoup
 
 # To get a webpage.
-def main():
+def getPhishState(id):
     url = "https://www.phishtank.com/phish_detail.php?phish_id="
-    res = requests.get(url+"15")
-    return res.text
+    res = requests.get(url+id)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    return soup.h2.text
+
+def main(id):
+    if getPhishState(id).find("Submission") > 0:
+        print("URL existed.")
+    else:
+        print("URL not exist.")
 
 if __name__ == '__main__':
-    print(main())
+    main("0")
+    main("15")
