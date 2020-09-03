f7da79d814eb347c0736f2c2729a05ba7527c731
RanlyX <et82122@gmail.com>
2020-09-03 23:41:42 +0800

    Get phish URL.

--- main.py
+++ main.py
@@ -4,17 +4,41 @@ import requests
 from bs4 import BeautifulSoup
 
 # To get a webpage.
-def getPhishState(id):
+def getPhishDetail(phishID):
     url = "https://www.phishtank.com/phish_detail.php?phish_id="
-    res = requests.get(url+id)
-    soup = BeautifulSoup(res.text, 'html.parser')
+    res = requests.get(url+phishID)
+    return res.text
+
+def getPhishState(webContent):
+    soup = BeautifulSoup(webContent, 'html.parser')
     return soup.h2.text
 
-def main(id):
-    if getPhishState(id).find("Submission") > 0:
-        print("URL existed.")
+def getPhishURL(webContent):
+    soup = BeautifulSoup(webContent, 'html.parser')
+    return soup.select("span b")[1].text
+
+def checkURLExist(webContent):
+    if getPhishState(webContent).find("Submission") > 0:
+        # print("URL existed.")
+        return True
     else:
-        print("URL not exist.")
+        # print("URL not exist.")
+        return False
+
+def main(phishID):
+    try:
+        webContent = getPhishDetail(phishID)
+    except requests.exceptions.Timeout as e:
+        # Maybe set up for a retry, or continue in a retry loop
+        raise SystemExit(e)
+    except requests.exceptions.TooManyRedirects as e:
+        # Tell the user their URL was bad and try a different one
+        raise SystemExit(e)
+    except requests.exceptions.RequestException as e:
+        # catastrophic error. bail.
+        raise SystemExit(e)
+    if checkURLExist(webContent):
+        print(getPhishURL(webContent))
 
 if __name__ == '__main__':
     main("0")
