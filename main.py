361b26a4d992df6d610c904abf3cc49a1f858482
RanlyX <et82122@gmail.com>
2020-09-07 13:37:10 +0800

    Fixed modules import bugs.

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 # !usr/bin/python
 # -*- coding:utf-8 -*-
-import sys
-from task import *
-from phish import *
+
+import phider.task as mtk
+import phider.phish as mp
 
 def main():
     for id_, ptid in enumerate(range(2,3)):
-        t = Task(id_=id_, phish=Phish(ptid)).getPhish()
-        # print(t)
-        p = t.phish
-        print(p)
+        t = mtk.Task(id_=id_, phish=mp.Phish(ptid)).getPhish()
+        print(t)
+        # p = t.phish
+        # print(p)
 
 if __name__ == '__main__':
     main()
